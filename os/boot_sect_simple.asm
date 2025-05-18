3cedee47eedfd8bcb6c11c29392e751fea42c727
frogprog <frogprog09@gmail.com>
2025-05-18 23:11:22 +0300

    boot made sure

--- os/boot_sect_simple.asm
+++ os/boot_sect_simple.asm
@@ -1,8 +1,6 @@
-; Infinite loop (e9 fd ff)
+; A simple boot sector program that loops forever
 loop:
-    jmp loop 
+    jmp loop
 
-; Fill with 510 zeros minus the size of the previous code
 times 510-($-$$) db 0
-; Magic number
 dw 0xaa55
