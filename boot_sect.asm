abe1242f38dc22d02dfc317a57f0de6af679fb30
frogprog <frogprog09@gmail.com>
2025-05-19 11:44:48 +0300

    Froggy OS

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,12 +1,20 @@
 mov ah, 0x0e ; tty mode
-mov al, 'H'
+mov al, 'F'
 int 0x10
-mov al, 'e'
+mov al, 'r'
 int 0x10
-mov al, 'l'
-int 0x10
-int 0x10 ; 'l' is still on al, remember?
 mov al, 'o'
+int 0x10
+mov al, 'g'
+int 0x10 
+int 0x10
+mov al, 'y'
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 'O'
+int 0x10
+mov al, 'S'
 int 0x10
 
 jmp $ ; jump to current address = infinite loop
